2e20d443db06622a173e7c5b917ca4aa86f57946
Helbertt Dalarme <hdalarme@yahoo.com.br>
2021-07-06 21:32:52 -0300

    first commit

---
+++ src/main/java/xyz/helbertt/ponto/model/JornadaTrabalho.java
@@ -0,0 +1,16 @@
+package xyz.helbertt.ponto.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaTrabalho {
+
+    private Long id;
+    private String descricao;
+
+}
